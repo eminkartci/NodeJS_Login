7d55f4dab74a21522bb368c60153e25d9286dac7
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-25 21:45:38 +0300

    Register - Login Pages / Get Post Form / Bcrypt / Url encoded

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 
+// CONST
+    // Express
 const express = require('express')
 const app = express()
 
-// Set view engine
+// SETs
+
+    // view engine
 app.set('view-engine','ejs');
 
+    // url encoded
+app.use(express.urlencoded({extended: false}))
+
+
+// GETS
 
 // Index Page
 app.get('/', (req,res) => {
  res.render('index.ejs', { name: ' Emin '})
 })
 
+app.get('/register', (req,res) => {
+    res.render('register.ejs')
+})
+
+app.get('/login', (req,res) => {
+    res.render('login.ejs')
+})
+
+
+// POSYS
+
+app.post('/register', (req,res) => {
+
+})
+
+app.post('/login', (req,res) => {
+
+})
 
 // Listen to Port 1122
 app.listen(1122,() => {
